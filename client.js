f220231b12def5bd1f38f424b450cd99faead1e6
Roger Clotet <roger.clotet@socialpoint.es>
2017-01-20 14:25:51 +0100

    Fixed and improved coding standards

--- client.js
+++ client.js
@@ -1,35 +1,37 @@
-/*  Copyright 2012-2016 Sven "underscorediscovery" Bergström
-    
-    written by : http://underscorediscovery.ca
-    written for : http://buildnewgames.com/real-time-multiplayer/
-    
-    MIT Licensed.
-*/
+"use strict";
 
-	//A window global for our game root variable.
+/*  Copyright 2012-2016 Sven "underscorediscovery" Bergström
+
+ written by : http://underscorediscovery.ca
+ written for : http://buildnewgames.com/real-time-multiplayer/
+
+ MIT Licensed.
+ */
+
+//A window global for our game root variable.
 var game = {};
 
-	//When loading, we store references to our
-	//drawing canvases, and initiate a game instance.
-window.onload = function(){
+//When loading, we store references to our
+//drawing canvases, and initiate a game instance.
+window.onload = function () {
 
-		//Create our game client instance.
-	game = new game_core();
+    //Create our game client instance.
+    game = new game_core();
 
-			//Fetch the viewport
-		game.viewport = document.getElementById('viewport');
-			
-			//Adjust their size
-		game.viewport.width = game.world.width;
-		game.viewport.height = game.world.height;
+    //Fetch the viewport
+    game.viewport = document.getElementById('viewport');
 
-			//Fetch the rendering contexts
-		game.ctx = game.viewport.getContext('2d');
+    //Adjust their size
+    game.viewport.width = game.world.width;
+    game.viewport.height = game.world.height;
 
-			//Set the draw style for the font
-		game.ctx.font = '11px "Helvetica"';
+    //Fetch the rendering contexts
+    game.ctx = game.viewport.getContext('2d');
 
-		//Finally, start the loop
-	game.update( new Date().getTime() );
+    //Set the draw style for the font
+    game.ctx.font = '11px "Helvetica"';
+
+    //Finally, start the loop
+    game.update(new Date().getTime());
 
 }; //window.onload
