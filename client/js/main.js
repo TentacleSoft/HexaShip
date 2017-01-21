722ca0233b3b24f6970d0f857ce3eead0d062ddd
ssebio <ssebio@gmail.com>
2017-01-21 17:59:50 +0100

    Recuperats els canvis de main.js...

--- client/js/main.js
+++ client/js/main.js
@@ -1,14 +1,37 @@
-const CANVAS_HEIGHT = 800;
-const CANVAS_WIDTH = 1400;
-const OFFSET_X = 100;
-const OFFSET_Y = CANVAS_HEIGHT/2;
+var socket = io.connect('/');
+//Now we can listen for that event
+socket.on('onconnected', function( data ) {
+    //Note that the data is the object we sent from the server, as is. So we can assume its id exists.
+    console.log( 'Connected successfully to the socket.io server. My server side ID is ' + data.id );
+});
 
-var game = new Phaser.Game(CANVAS_WIDTH, CANVAS_HEIGHT, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+availableWidth = window.innerWidth;
+availableHeight = window.innerHeight;
+
+var canvasHeight = availableHeight;
+var canvasWidth = availableWidth;
+if (availableWidth / availableHeight > 700 / 400) {
+    canvasWidth = availableHeight * 700 / 400;
+} else {
+    canvasHeight = availableWidth * 400 / 700;
+}
+
+const SCALE = canvasWidth / 1400;
+const OFFSET_X = 100 * SCALE;
+const OFFSET_Y = canvasHeight / 2;
+
+var game = new Phaser.Game(canvasWidth, canvasHeight, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
     function preload () {
 
+        //game assets
         game.load.image('cell', '../assets/hexagon_border.png');
         game.load.image('redship', '../assets/ship1.png');
+
+        //UI assets
+        game.load.image('button', '../assets/button.png');
+        game.load.image('button_pressed', '../assets/button_pressed.png');
+        game.load.image('misile_button', '../assets/misile_button.png');
 
         // TODO give attribution notice for Ivan Voirol : http://opengameart.org/content/basic-map-32x32-by-silver-iv
         game.load.spritesheet('beach', 'assets/beachmap.png', 32, 32);
@@ -30,8 +53,9 @@ var game = new Phaser.Game(CANVAS_WIDTH, CANVAS_HEIGHT, Phaser.AUTO, '', { prelo
         	for (let j = 0; j < size_y; j++){
                 let type = grid.getCell(i,j);
                 if (type != -1) {
-                    let coords = position2dToPixels(i,j);
+                    let coords = hex2pixCoords(i,j);
                     let cell = game.add.sprite(coords.x, coords.y, 'cell');
+                    cell.scale.setTo(SCALE);
                     setAnchorMid(cell);
                 }
         	}
@@ -41,29 +65,33 @@ var game = new Phaser.Game(CANVAS_WIDTH, CANVAS_HEIGHT, Phaser.AUTO, '', { prelo
         player = new Player(0,0);
 
         ship = game.add.sprite(0, 0, 'redship');
-		setAnchorMid(ship);
+        ship.scale.setTo(SCALE);
+    		setAnchorMid(ship);
 
 		cursors = game.input.keyboard.createCursorKeys();
     }
 
     function createBackground() {
       let background = [];
-      for (i = 0; i < CANVAS_WIDTH/32; ++i) {
-                background[i] = [];
-                for (j = 0; j < CANVAS_HEIGHT/32; ++j) {
-                    background[i][j] = game.add.sprite(32 * i, j * 32, 'beach')
-                    var frames = [];
-                    if (i % 3 == 0) {
-                        frames = [496, 496, 498];
-                    } else if (i % 3 == 1) {
-                        frames = [496, 498, 496];
-                    } else {
-                        frames = [498, 496, 496];
-                    }
-                    background[i][j].animations.add('water', frames, 0.5, true);
-                    background[i][j].animations.play('water');
-                }
-            }
+      for (i = 0; i <= window.innerWidth/32; ++i) {
+          background[i] = [];
+          for (j = 0; j <= window.innerHeight/32; ++j) {
+              background[i][j] = game.add.sprite(32 * i, j * 32, 'beach')
+              var frames = [];
+              if (i % 3 == 0) {
+                  frames = [496, 496, 498];
+              } else if (i % 3 == 1) {
+                  frames = [496, 498, 496];
+              } else {
+                  frames = [498, 496, 496];
+              }
+              background[i][j].animations.add('water', frames, 0.5, true);
+              background[i][j].animations.play('water');
+          }
+      }
+    }
+
+    function createUI() {
     }
 
     function update () {
@@ -105,15 +133,15 @@ var game = new Phaser.Game(CANVAS_WIDTH, CANVAS_HEIGHT, Phaser.AUTO, '', { prelo
     	sprite.anchor.y = 0.5;
     }
 
-    function position2dToPixels(hex_x,hex_y) {
+    function hex2pixCoords(hex_x,hex_y) {
     	let pix_coords = {};
-    	pix_coords.x = Cell.WIDTH*0.75 * (hex_y + hex_x) + OFFSET_X;
-    	pix_coords.y = Cell.HEIGHT*0.5 * (-hex_x + hex_y) + OFFSET_Y;
+    	pix_coords.x = SCALE * Cell.WIDTH*0.75 * (hex_y + hex_x) + OFFSET_X;
+    	pix_coords.y = SCALE * Cell.HEIGHT*0.5 * (-hex_x + hex_y) + OFFSET_Y;
     	return pix_coords;
     }
     function position2dToPixels2(hex_x,hex_y){
 		let pix_coords = {};
-		pix_coords.x = Cell.HEIGHT * hex_x +CANVAS_WIDTH/2-20;
-		pix_coords.y = Cell.HEIGHT * hex_y + CANVAS_HEIGHT/2;
+		pix_coords.x = SCALE *(Cell.HEIGHT * hex_x-20) +canvasWidth/2;
+		pix_coords.y = SCALE * (Cell.HEIGHT * hex_y) + availableHeight/2;
 		return pix_coords;
     }
