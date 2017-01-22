6eb29f00e6f3a4f80193669895b8b97484729db4
Roger Clotet <roger.clotet@socialpoint.es>
2017-01-22 01:35:49 +0100

    Limit shoots

--- server/order_queue.js
+++ server/order_queue.js
@@ -6,10 +6,19 @@ class OrderQueue {
     }
 
     push(order) {
-        // TODO avoid pushing orders during turn processing
+        // TODO avoid pushing orders during turn processing?
 
         if (this.orders.length >= size) {
             return;
+        }
+
+        // Limit to 1 shoot at a time
+        if (order.type === 'shoot') {
+            for (var i in this.orders) {
+                if (orders[i].type === 'shoot') {
+                    return;
+                }
+            }
         }
 
         this.orders.push(order);
